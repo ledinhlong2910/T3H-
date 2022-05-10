93adb91354fb251573ba7449857898b5df1e98ff
LongT3H <fc.skyofme1@gmail.com>
2022-05-10 15:12:26 +0700

    baitapbuoi2

---
+++ src/buoi1/cat.java
@@ -0,0 +1,4 @@
+package buoi1;
+
+public class cat {
+}
---
+++ src/buoi1/runner.java
@@ -0,0 +1,7 @@
+package buoi1;
+
+public class runner {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
